c7ad7e89e490c1ad0b2e6eb04175db44b5d0172c
Song Jin <boulevardaed@gmail.com>
2017-05-13 17:19:52 -0700

    Added date picker and related styles

--- ExpenseTracker/app/src/main/java/com/songjin/expensetracker/MainActivity.java
+++ ExpenseTracker/app/src/main/java/com/songjin/expensetracker/MainActivity.java
@@ -1,5 +1,7 @@
 package com.songjin.expensetracker;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.graphics.Rect;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
@@ -10,7 +12,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.BindString;
 import butterknife.BindView;
@@ -19,15 +28,15 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
 
     private BottomSheetBehavior behavior;
+    private DatePickerDialog.OnDateSetListener dateListener;
+    private Calendar calendar;
 
-    @BindView(R.id.main_toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.addExpenseBottomSheet)
-    FrameLayout bottomSheet;
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
-    @BindString(R.string.app_name)
-    String appName;
+    @BindView(R.id.main_toolbar) Toolbar toolbar;
+    @BindView(R.id.addExpenseBottomSheet) FrameLayout bottomSheet;
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.edittext_date) EditText editTextDate;
+
+    @BindString(R.string.app_name) String appName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +44,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        // toolbar setup
         setSupportActionBar(toolbar);
         toolbar.setTitle(appName);
 
+        // bottom layout setup
         behavior = BottomSheetBehavior.from(bottomSheet);
         behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
         behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
@@ -45,8 +56,7 @@ public class MainActivity extends AppCompatActivity {
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
                 if (newState == BottomSheetBehavior.STATE_DRAGGING) {
                     behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                }
-                if (newState == BottomSheetBehavior.STATE_SETTLING) {
+                } else if (newState == BottomSheetBehavior.STATE_HIDDEN) {
                     fab.show();
                 }
             }
@@ -57,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // fab setup
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,10 +75,28 @@ public class MainActivity extends AppCompatActivity {
                     behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
                     fab.hide();
                 } else {
-                    behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
                 }
             }
         });
+
+        // date picker setup
+        calendar = Calendar.getInstance();
+
+        dateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                // update label
+                String myFormat = "MM/dd/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                editTextDate.setText(sdf.format(calendar.getTime()));
+            }
+        };
     }
 
     @Override
@@ -94,10 +123,28 @@ public class MainActivity extends AppCompatActivity {
                 bottomSheet.getGlobalVisibleRect(outRect);
 
                 if(!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
-                    behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                    onDismissClicked(getWindow().getDecorView().getRootView());
                 }
             }
         }
         return super.dispatchTouchEvent(event);
+    }
+
+    public void onOkClicked(View v) {
+        onDismissClicked(v);
+    }
+
+    public void onDismissClicked(View v) {
+        behavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+        }
+    }
+
+    public void onDateClicked(View v) {
+        new DatePickerDialog(MainActivity.this, dateListener, calendar.get(Calendar.YEAR),
+                             calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+                            .show();
     }
 }
